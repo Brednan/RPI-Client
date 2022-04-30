27e8c8d6dbca134ecd9abb9c95cb7ca10636f094
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2022-04-29 23:51:52 -0700

    I created a function to both request for an image (the footage is what it will be used for), and to receive it

--- client.py
+++ client.py
@@ -1,11 +1,15 @@
+from ctypes import sizeof
 import socket
 import time
+import io
+from PIL import Image
 
 class Client:
     def __init__(self, HOST, PORT):
         self.HOST = HOST
         self.PORT = PORT
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.settimeout(1.8)
 
     #This function was made to test if the server was working
     def test_request(self):
@@ -18,3 +22,18 @@ class Client:
     def send_request(self, body:bytes):
         self.client.connect((self.HOST, self.PORT))
         self.client.sendall(body)
+    
+    def request_footage(self):
+        self.client.connect((self.HOST, self.PORT))
+        self.client.sendall(b'Footage')
+
+        footage_chunk = self.client.recv(2048)
+        
+        output_file = open('test.png', 'wb')
+        
+        while footage_chunk:
+            output_file.write(footage_chunk)
+            try:
+                footage_chunk = self.client.recv(2048)
+            except:
+                break
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from client import Client
 
 client = Client('192.168.1.68', 65432)
 
-client.send_request(b'Stop')
+client.request_footage()
