ba04e1c20150d55a2d49890cff961baccbebf608
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2022-04-30 18:12:54 -0700

    added widgets to call the function to send a request when pressed. For example the forward button will send a request to the server telling it to move forward

--- client.py
+++ client.py
@@ -8,32 +8,35 @@ class Client:
     def __init__(self, HOST, PORT):
         self.HOST = HOST
         self.PORT = PORT
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.settimeout(1.8)
 
     #This function was made to test if the server was working
     def test_request(self):
-        self.client.connect((self.HOST, self.PORT))
-        self.client.sendall(b'Footage')
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((self.HOST, self.PORT))
+        client.sendall(b'Footage')
         time.sleep(1)
-        return self.client.recv(1024)
+        return client.recv(1024)
 
-    #This function sends a request to the server for it to move the motors forward
+    #This function sends a request to the server with a specified message
     def send_request(self, body:bytes):
-        self.client.connect((self.HOST, self.PORT))
-        self.client.sendall(body)
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((self.HOST, self.PORT))
+        client.sendall(body)
     
+    #This function requests for the pictures that are taken by the Pi-Cam
     def request_footage(self):
-        self.client.connect((self.HOST, self.PORT))
-        self.client.sendall(b'Footage')
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(1.8)
+        client.connect((self.HOST, self.PORT))
+        client.sendall(b'Footage')
 
-        footage_chunk = self.client.recv(2048)
+        footage_chunk = client.recv(2048)
         
-        output_file = open('test.png', 'wb')
+        output_file = open('./Vision/vision.png', 'wb')
         
         while footage_chunk:
             output_file.write(footage_chunk)
             try:
-                footage_chunk = self.client.recv(2048)
+                footage_chunk = client.recv(2048)
             except:
                 break
--- gui.py
+++ gui.py
@@ -10,9 +10,25 @@ class GUI:
 
     def gui(self):
         self.main = MainWindow(self.size, self.bg)
+        self.main.window.protocol('WM_DELETE_WINDOW', lambda: self.on_close())
+        
         self.forward_button = ForwardButton(self.main.window, (400, 50))
+        self.forward_button.button.config(command=lambda: self.client.send_request(b'Forward'))
+
+        self.stop_button = StopButton(self.main.window, (400, 120))
+        self.stop_button.button.config(command=lambda: self.client.send_request(b'Stop'))
+
+        self.right_button = RightButton(self.main.window, (550, 120))
+        self.right_button.button.config(command=lambda: self.client.send_request(b'Right'))
+
+        self.stop_button = LeftButton(self.main.window, (250, 120))
+        self.stop_button.button.config(command=lambda: self.client.send_request(b'Left'))
 
         self.main.window.mainloop()
+    
+    def on_close(self):
+        self.client.send_request(b'Stop')
+        self.main.window.destroy()
 
 
 class MainWindow:
@@ -24,5 +40,23 @@ class MainWindow:
 
 class ForwardButton:
     def __init__(self, master, pos:tuple):
-        self.button = tk.Button(master, text='Forwards', font=('default', 20), bg='#00ADBB', fg='white')
+        self.button = tk.Button(master, text='Forward', font=('default', 20), bg='#00ADBB', fg='white', width=8)
+        self.button.place(x=pos[0], y=pos[1], anchor=tk.CENTER)
+
+
+class StopButton:
+    def __init__(self, master, pos:tuple):
+        self.button = tk.Button(master, text='Stop', font=('default', 20), bg='#BB0000', fg='white', width=8)
+        self.button.place(x=pos[0], y=pos[1], anchor=tk.CENTER)
+
+
+class RightButton:
+    def __init__(self, master, pos:tuple):
+        self.button = tk.Button(master, text='Right', font=('default', 20), bg='#00ADBB', fg='white', width=8)
+        self.button.place(x=pos[0], y=pos[1], anchor=tk.CENTER)
+
+
+class LeftButton:
+    def __init__(self, master, pos:tuple):
+        self.button = tk.Button(master, text='Left', font=('default', 20), bg='#00ADBB', fg='white', width=8)
         self.button.place(x=pos[0], y=pos[1], anchor=tk.CENTER)
